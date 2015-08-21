4e1a4cedde2bd0ee145858b9e79ef1d6e5e6d71c
Intel 478 <intel_478@hotmail.com>
2015-08-21 18:21:49 +0200

    bugfix: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=58016
    NDK_TOOLCHAIN_VERSION
    
    Define this variable as either 4.9 or 4.8 to select a version of the GCC compiler. Version 4.9 is the default for 64-bit ABIs, and 4.8 is the default for 32-bit ABIs.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,6 @@
-APP_STL := gnustl_static
+NDK_TOOLCHAIN_VERSION := 4.9
+APP_OPTIM    := debug #release
+APP_STL      := gnustl_static #stlport_static #gabi++_static #c++_static
+APP_MODULES  := zipalign aapt expat libpng libcutils libutils libhost #aidl
+APP_ABI      := armeabi armeabi-v7a x86 mips
 APP_PLATFORM := android-18
